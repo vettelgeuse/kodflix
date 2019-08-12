08c816c465cd8b6e3a25d10ce4e22cf2f90172b2
Sebastian <mastropieros@gmail.com>
2019-08-12 12:29:17 +0100

    Add 6 TV show titles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import logo from './enlightenment_guaranteed.jpg';
+import movie1 from './enlightenment_guaranteed.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <img src={logo} alt='Enlightenment Guaranteed' />
+     <img src={movie1} alt='Enlightenment Guaranteed' />
+     <br /><br /><br /> 
+     <div className="container">
+        <div className='movie_pic'>ReactJs</div>
+        <div className='movie_pic'>HTML5</div>
+        <div className='movie_pic'>CSS3</div>
+     </div>
+     <div className="container">
+        <div className='movie_pic'>Node.js</div>
+        <div className='movie_pic'>MongoDB</div>
+        <div className='movie_pic'>Heroku</div>
+     </div> 
     </div>
   );
 }
