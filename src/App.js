39ce7df6ecc608b3437168990379e29f40e0b15c
Sebastian <mastropieros@gmail.com>
2019-08-14 12:37:57 +0100

    changed  movie covers path and replaced text with covers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import movie1 from './enlightenment_guaranteed.jpg';
+import movie1 from './images/enlightenment_guaranteed.jpg';
+import movie2 from './images/circulo_polar.jpg';
+import movie3 from './images/dolce_vita.jpg';
+import movie4 from './images/gitanos.jpg';
+import movie5 from './images/rashomon.jpg';
+import movie6 from './images/sita_blues.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <img src={movie1} alt='Enlightenment Guaranteed' />
-     <br /><br /><br /> 
-     <div className="container">
-        <div className='movie_pic'> <h1>ReactJs</h1> </div>
-        <div className='movie_pic'> <h1>HTML5</h1> </div>
-        <div className='movie_pic'> <h1>CSS3</h1> </div>
-     </div>
-     <div className="container">
-        <div className='movie_pic'><h1>Node.js</h1></div>
-        <div className='movie_pic'><h1>MongoDB</h1></div>
-        <div className='movie_pic'><h1>Heroku</h1></div>
-     </div> 
+      <div className="container">
+        <div className='movie_pic'>
+          <img src={movie1} alt='Enlightenment Guaranteed' />
+        </div>
+        <div className='movie_pic'>
+          <img src={movie2} alt='Los amantes del Circulo Polar' />
+        </div>
+        <div className='movie_pic'>
+          <img src={movie3} alt='La Dolce Vita' />
+        </div>
+      </div>
+      <div className="container">
+        <div className='movie_pic'>
+          <img src={movie4} alt='Tiempo de Gitanos' />
+        </div>
+        <div className='movie_pic'>
+          <img src={movie5} alt='Rashomon' />
+        </div>
+        <div className='movie_pic'>
+          <img src={movie6} alt='Sita sings the Blues' />
+        </div>
+      </div>
     </div>
   );
 }
